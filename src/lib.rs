3b614c2ef31831328d1bdf2da246079ef7102d94
Wojciech Kępka <wojtek.kepka@protonmail.com>
2021-03-02 09:00:23 +0100

    Use clap for argument parsing

--- src/lib.rs
+++ src/lib.rs
@@ -11,9 +11,11 @@ use thiserror::Error;
 pub enum Error {
     #[error("tag already exists")]
     TagExists,
+    #[error("tag doesn't exist")]
+    TagNotFound,
     #[error("provided file doesn't exists")]
     FileNotFound,
-    #[error("unexpected error - `{0}`")]
+    #[error("error: {0}")]
     Other(String),
     #[error("provided string was invalid")]
     InvalidString(#[from] std::ffi::NulError),
@@ -28,7 +30,10 @@ impl From<io::Error> for Error {
         match err.kind() {
             io::ErrorKind::NotFound => Error::FileNotFound,
             io::ErrorKind::AlreadyExists => Error::TagExists,
-            _ => Error::Other(err.to_string()),
+            _ => match err.raw_os_error() {
+                Some(61) => Error::TagNotFound,
+                _ => Error::Other(err.to_string()),
+            },
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,68 @@
+use clap::Clap;
+use serde_json::Value;
+use std::path::PathBuf;
+
 use rutag::{get_xattr, list_xattrs, set_xattr};
 
+#[derive(Clap)]
+#[clap(version = "0.1.0", author = "Wojciech Kępka <wojciech@wkepka.dev>")]
+struct RutagOpts {
+    #[clap(subcommand)]
+    pub cmd: RutagCmd,
+}
+
+#[derive(Clap)]
+enum RutagCmd {
+    List {
+        path: PathBuf,
+        #[clap(short, long)]
+        pretty: bool,
+    },
+    Set {
+        path: PathBuf,
+        key: String,
+        value: String,
+    },
+    Get {
+        path: PathBuf,
+        key: String,
+    },
+}
+
 fn main() {
-    let mut args = std::env::args().into_iter().skip(1);
+    let opts = RutagOpts::parse();
 
-    let op = args.next().expect("operation");
-    let filename = args.next().expect("filename");
-
-    match &op[..] {
-        "get" => {
-            let key = args.next().expect("key");
-            match get_xattr(&filename, key) {
-                Ok(tag) => println!("{} - {}", filename, tag),
-                Err(e) => eprintln!("{}", e),
-            }
-        }
-        "list" => match list_xattrs(&filename) {
-            Ok(attrs) => println!("{:#?}", attrs),
+    match opts.cmd {
+        RutagCmd::Get { path, key } => match get_xattr(path.as_path(), &key) {
+            Ok(tag) => println!("{}={}", key, tag),
             Err(e) => eprintln!("{}", e),
         },
-        "set" => {
-            let key = args.next().expect("key");
-            let value = args.next().expect("value");
+        RutagCmd::List { path, pretty } => match list_xattrs(path.as_path()) {
+            Ok(attrs) => {
+                let attrs = attrs
+                    .into_iter()
+                    .map(|(k, v)| (k, Value::from(v.as_str())))
+                    .collect::<serde_json::Map<String, Value>>();
 
-            if let Err(e) = set_xattr(filename, key, value) {
+                let display = if pretty {
+                    serde_json::to_string_pretty(&attrs)
+                } else {
+                    serde_json::to_string(&attrs)
+                };
+
+                match display {
+                    Ok(display) => {
+                        println!("{}", display);
+                    }
+                    Err(e) => eprintln!("failed to serialize attributes - {}", e),
+                }
+            }
+            Err(e) => eprintln!("{}", e),
+        },
+        RutagCmd::Set { path, key, value } => {
+            if let Err(e) = set_xattr(path.as_path(), &key, &value) {
                 eprintln!("{}", e);
             }
         }
-        _ => {}
     }
 }
