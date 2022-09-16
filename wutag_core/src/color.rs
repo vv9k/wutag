bf6246e9751cd3a8f1415365d787757a4003613d
Wojciech Kępka <wojciech@wkepka.dev>
2022-09-16 18:45:56 +0200

    Reexport colored items publicly in core module

--- wutag_core/src/color.rs
+++ wutag_core/src/color.rs
@@ -1,6 +1,6 @@
 //! Utility functions used through this crate and by the main executable
-use colored::Color;
 use colored::Color::*;
+pub use colored::{control, Color};
 
 use crate::{Error, Result};
 
